8d13f681ae20ca9b9f22baa1b7af97f6bf390a93
Lastlilith <Lastlilith.2014@gmail.com>
2023-01-05 14:39:04 +0900

    Setup navigation

--- app/src/main/java/com/imnidasoftware/animeapp/MainActivity.kt
+++ app/src/main/java/com/imnidasoftware/animeapp/MainActivity.kt
@@ -3,14 +3,21 @@ package com.imnidasoftware.animeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.imnidasoftware.animeapp.navigation.SetupNavGraph
 import com.imnidasoftware.animeapp.ui.theme.AnimeAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AnimeAppTheme {
-
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
     }
---
+++ app/src/main/java/com/imnidasoftware/animeapp/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.imnidasoftware.animeapp.util
+
+object Constants {
+    const val DETAILS_ARGUMENT_KEY = "heroId"
+}
