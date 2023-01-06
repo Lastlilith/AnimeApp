9bf926e647a91d6ede402f126be22f346557b9be
Lastlilith <Lastlilith.2014@gmail.com>
2023-01-06 18:57:03 +0900

    Added home screen top bar

--- app/src/main/java/com/imnidasoftware/animeapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/imnidasoftware/animeapp/presentation/screens/home/HomeScreen.kt
@@ -1,8 +1,15 @@
 package com.imnidasoftware.animeapp.presentation.screens.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            HomeTopBar(onSearchClicked = {})
+        }
+    ) {
 
+    }
 }
