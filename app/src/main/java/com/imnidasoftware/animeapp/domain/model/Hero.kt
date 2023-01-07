331cd0e680fda709dba36de3b536c9e1a28623de
Lastlilith <lastlilith.2014@gmail.com>
2023-01-07 12:58:24 +0900

    Created ApiResponse.kt

--- app/src/main/java/com/imnidasoftware/animeapp/domain/model/Hero.kt
+++ app/src/main/java/com/imnidasoftware/animeapp/domain/model/Hero.kt
@@ -3,7 +3,9 @@ package com.imnidasoftware.animeapp.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.imnidasoftware.animeapp.util.Constants.HERO_DATABASE_TABLE
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Entity(tableName = HERO_DATABASE_TABLE)
 data class Hero(
     @PrimaryKey(autoGenerate = false)
