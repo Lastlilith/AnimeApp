644227283e10506f59bf05dd25fc5633fef22a9e
Lastlilith <lastlilith.2014@gmail.com>
2023-01-09 14:29:49 +0900

    Created half filled star widget

--- app/src/main/java/com/imnidasoftware/animeapp/presentation/components/RatingWidget.kt
+++ app/src/main/java/com/imnidasoftware/animeapp/presentation/components/RatingWidget.kt
@@ -1,13 +1,14 @@
 package com.imnidasoftware.animeapp.presentation.components
 
-import android.media.Rating
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.vector.PathParser
@@ -15,30 +16,32 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.imnidasoftware.animeapp.R
+import com.imnidasoftware.animeapp.ui.theme.LightGray
 import com.imnidasoftware.animeapp.ui.theme.StarColor
 
 @Composable
 fun RatingWidget(
     modifier: Modifier,
-    raiting: Double
+    raiting: Double,
+    scaleFactor: Float = 3f
 ) {
 
     val starPathString = stringResource(id = R.string.star_path)
     val starPath = remember {
         PathParser().parsePathString(pathData = starPathString).toPath()
     }
-    val starOathBounds = remember {
+    val starPathBounds = remember {
         starPath.getBounds()
     }
 
-    FilledStar(starPath = starPath, starPathBounds = starOathBounds)
+    FilledStar(starPath = starPath, starPathBounds = starPathBounds, scaleFactor = scaleFactor)
 }
 
 @Composable
 fun FilledStar(
     starPath: Path,
     starPathBounds: Rect,
-    scaleFactor: Float = 1f
+    scaleFactor: Float
 ) {
     Canvas(modifier = Modifier.size(24.dp)) {
         val canvasSize = size
@@ -61,8 +64,67 @@ fun FilledStar(
     }
 }
 
+@Composable
+fun HalFFilledStar(
+    starPath: Path,
+    starPathBounds: Rect,
+    scaleFactor: Float
+) {
+    Canvas(modifier = Modifier.size(24.dp)) {
+        val canvasSize = size
+        scale(scale = scaleFactor) {
+            val pathWidth = starPathBounds.width
+            val pathHeight = starPathBounds.height
+            val left = (canvasSize.width / 2f) - (pathWidth / 1.7f)
+            val top = (canvasSize.height / 2f) - (pathHeight / 1.7f)
+            translate(
+                left = left,
+                top = top
+            ) {
+                drawPath(
+                    path = starPath,
+                    color = LightGray.copy(alpha = 0.5f)
+                )
+                clipPath(path = starPath) {
+                    drawRect(
+                        color = StarColor,
+                        size = Size(
+                            width = starPathBounds.maxDimension / 1.7f,
+                            height = starPathBounds.maxDimension * scaleFactor
+                        )
+                    )
+                }
+            }
+        }
+
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun FilledStarPreview() {
-    RatingWidget(modifier = Modifier, raiting = 1.0)
+    val starPathString = stringResource(id = R.string.star_path)
+    val starPath = remember {
+        PathParser().parsePathString(pathData = starPathString).toPath()
+    }
+    val starOathBounds = remember {
+        starPath.getBounds()
+    }
+
+    FilledStar(starPath = starPath, starPathBounds = starOathBounds, scaleFactor = 3f)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HalfFilledStarPrev() {
+
+    val starPathString = stringResource(id = R.string.star_path)
+    val starPath = remember {
+        PathParser().parsePathString(pathData = starPathString).toPath()
+    }
+    val starOathBounds = remember {
+        starPath.getBounds()
+    }
+
+    HalFFilledStar(starPath = starPath, starPathBounds = starOathBounds, scaleFactor = 3f)
 }
