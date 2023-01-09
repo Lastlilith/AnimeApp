0102b0e73aa119c2cb18703106f9c109c0271d73
Lastlilith <lastlilith.2014@gmail.com>
2023-01-09 17:54:43 +0900

    Implemented rating widget logic

--- app/src/main/java/com/imnidasoftware/animeapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/imnidasoftware/animeapp/presentation/screens/home/HomeScreen.kt
@@ -1,12 +1,18 @@
 package com.imnidasoftware.animeapp.presentation.screens.home
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.imnidasoftware.animeapp.presentation.components.RatingWidget
+import com.imnidasoftware.animeapp.ui.theme.LARGE_PADDING
 
 @Composable
 fun HomeScreen(
+    navController: NavHostController,
     homeViewModel: HomeViewModel = hiltViewModel()
 ) {
     val allHeroes = homeViewModel.getAllHeroes.collectAsLazyPagingItems()
@@ -16,6 +22,9 @@ fun HomeScreen(
             HomeTopBar(onSearchClicked = {})
         }
     ) {
-
+        RatingWidget(
+            modifier = Modifier.padding(all = LARGE_PADDING),
+            rating = 4.5
+        )
     }
 }
